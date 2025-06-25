1bb31e552b2f7d8ce9cc241b0e58699c74f40025
Stephanie Fuda <stephaniefuda@proton.me>
2025-06-25 17:57:16 -0400

    rebuild on JS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
-import type { GatsbyConfig } from "gatsby";
-
-const config: GatsbyConfig = {
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
   siteMetadata: {
     title: `EmpressInverted`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -30,5 +27,3 @@ const config: GatsbyConfig = {
     __key: "pages"
   }]
 };
-
-export default config;
